b1153f663eb07b0c069cadc35bad6ccd559a0574
Youdede <17805453+udede@users.noreply.github.com>
2021-09-01 22:20:13 +0200

    Update solution.js

--- optimal-assignments/solution.js
+++ optimal-assignments/solution.js
@@ -1,36 +1,38 @@
 
-function costMatch(matchAssignment, machineCosts) {
-  return matchAssignment.reduce((acc, cur, idx) => acc + machineCosts[idx][cur], 0);
+function getCost(m, mCosts) {
+  return m.reduce(
+    (acc, cur, idx) => acc + mCosts[idx][cur], 0);
 }
 
-function* generateMatches(items) {
-  if (items.length === 1) {
-    yield items;
+function* getMatches(els) {
+  if (els.length === 1) {
+    yield els;
     return;
   }
 
-  for (let match of generateMatches(items.slice(1))) {
-    for (let i = 0; i <= match.length; i ++) {
-      yield match.slice(0, i).concat([items[0]]).concat(match.slice(i));
+  for (let m of getMatches(els.slice(1))) {
+    for (let i = 0; i <= m.length; i ++) {
+      yield m.slice(0, i)
+      .concat([els[0]])
+      .concat(m.slice(i));
     }
   }
 }
 
 function OptimalAssignments(strArr) { 
-    const machineCosts = strArr.map(line => line.replace(/^\(|\)$/g, ''))
+    const mCosts = strArr.map(line => line.replace(/^\(|\)$/g, ''))
     .map(line => line.split(/,/)
     .map(i => parseInt(i, 10)));
-    let bestMatchCost = Number.MAX_SAFE_INTEGER;
-    let bestMatchAssignments = undefined;
+    let bestCost = Number.MAX_SAFE_INTEGER;
+    let bestM = undefined;
 
-    for (var match of generateMatches(machineCosts.map((_, i) => i))) {
-        const cost = costMatch(match, machineCosts);
-        if (cost < bestMatchCost) {
-            bestMatchCost = cost;
-            bestMatchAssignments = match.slice(0);
+    for (var m of getMatches(mCosts.map((_, i) => i))) {
+        const cost = getCost(m, mCosts);
+        if (cost < bestCost) {
+            bestCost = cost;
+            bestM = m.slice(0);
         }
     }
 
-    return bestMatchAssignments.map((t, i) => `(${i+1}-${t+1})`).join('');
-
-}    
+    return bestM.map((t, i) => `(${i+1}-${t+1})`).join('');
+}   
